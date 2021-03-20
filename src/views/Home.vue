04c814a82a2450f783b60782f9314cc5f9f704f5
zidyazid <darkyazid132@gmail.com>
2021-03-20 15:20:03 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderSalwa/>
+    <HeroSalwa/>
+    <BannerWomanSalwa/>
+    <InstagramPhotoSalwa/>
+    <PartnerSalwa/>
+    <FooterSalwa/>
+  </div>
+</template>
+
+<script>
+
+// import carousel from 'vue-owl-carousel'
+import HeaderSalwa from '@/components/HeaderSalwa.vue'
+import HeroSalwa from '@/components/HeroSalwa.vue'
+import BannerWomanSalwa from '@/components/BannerWomanSalwa'
+import InstagramPhotoSalwa from '@/components/InstagramPhotoSalwa'
+import FooterSalwa from '@/components/FooterSalwa'
+import PartnerSalwa from '@/components/PartnerSalwa'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderSalwa,
+    HeroSalwa,
+    BannerWomanSalwa,
+    InstagramPhotoSalwa,
+    PartnerSalwa,
+    FooterSalwa
+  }
+}
+</script>
+
+  
---
+++ src/views/ProductSalwa.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="product">
+    <HeaderSalwa/>
+        <!-- letak product -->
+            <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text product-more">
+                        <!-- <a href="./home.html"><i class="fa fa-home"></i></a> -->
+                        <router-link to="/"> Home</router-link>
+                        <span>Detail</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="product-pic-zoom">
+                                <!-- dikarenakan penggunaan vue js maka src perlu dibinding -->
+                                <img class="product-big-img" :src="photo" alt="" />
+                            </div>
+                            <div class="product-thumbs">
+                                <carousel class="product-thumbs-track ps-slider">
+                                    <div class="pt" @click="changeImage(thumbnail[0])" :class="thumbnail[0] == photo ? 'active' : ''">
+                                        <img src="img/mickey1.jpg" alt="" />
+                                    </div>
+
+                                    <div class="pt" @click="changeImage(thumbnail[1])" :class="thumbnail[1] == photo ? 'active' : ''">
+                                        <img src="img/mickey2.jpg" alt="" />
+                                    </div>
+                                    <div class="pt" @click="changeImage(thumbnail[2])" :class="thumbnail[2] == photo ? 'active' : ''">
+                                        <img src="img/mickey3.jpg" alt="" />
+                                    </div>
+                                    <div class="pt" @click="changeImage(thumbnail[3])" :class="thumbnail[3] == photo ? 'active' : ''">
+                                        <img src="img/mickey4.jpg" alt="" />
+                                    </div>
+
+                                   
+                                </carousel>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="product-details">
+                                <div class="pd-title">
+                                    <span>oranges</span>
+                                    <h3>Pure Pineapple</h3>
+                                </div>
+                                <div class="pd-desc">
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
+                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
+                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
+                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
+                                    </p>
+                                    <h4>$495.00</h4>
+                                </div>
+                                <div class="quantity">
+                                    <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+     <RelatedProductSalwa/>
+    <!-- Product Shop Section End -->
+    <FooterSalwa/>
+  </div>
+</template>
+
+<script>
+
+import carousel from 'vue-owl-carousel'
+import HeaderSalwa from '@/components/HeaderSalwa.vue'
+// import HeroSalwa from '@/components/HeroSalwa.vue'
+// import BannerWomanSalwa from '@/components/BannerWomanSalwa'
+// import InstagramPhotoSalwa from '@/components/InstagramPhotoSalwa'
+import FooterSalwa from '@/components/FooterSalwa'
+import RelatedProductSalwa from '@/components/RelatedProductSalwa'
+// import PartnerSalwa from '@/components/PartnerSalwa'
+
+export default {
+  name: 'product',
+  components: {
+    HeaderSalwa,
+    FooterSalwa,
+    RelatedProductSalwa,
+    carousel
+  },
+  // function data untuk menyimpan data
+  data() {
+    //   mengembalikan nilai data
+      return {
+        //   properti yang menyimpan nilai
+          photo: "img/mickey1.jpg",
+          // apa bila ter jadi error unexpected token perhatikan simbol yang digunakan apakah sesuai dengan standar dari vue atau tidak
+          thumbnail: [
+              "img/mickey1.jpg",
+              "img/mickey2.jpg",
+              "img/mickey3.jpg",
+              "img/mickey4.jpg"
+          ]
+      }
+  },
+//   digunakan untuk menghendle event yang ada pada js seperti onclick dan lain sebagainya
+  methods: {
+      changeImage(urlImage){
+          this.photo = urlImage;
+      }
+  }
+};
+</script>
+
+<style scoped>
+    .product-thumbs .pt {
+        margin-right: 10px;
+    }
+</style>
+
+  
